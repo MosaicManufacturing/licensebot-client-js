45fcfca23cc639a2ecbeb4ccc096e858350421f3
Brandon Bloch <brandon@bloch.ca>
2021-12-13 12:31:45 -0500

    CLI usage and configurable license bundle location

--- src/index.js
+++ src/index.js
@@ -2,21 +2,34 @@ const fs = require('fs/promises');
 const path = require('path');
 const getLicenses = require('./get-licenses');
 
-const licensesPath = path.join(__dirname, 'licenses.json');
+const defaultBundlePath = path.join(process.cwd(), 'licenses.json');
+
+// const licensesPath = path.join(__dirname, 'licenses.json');
+
+const help = () => {
+  console.log('licensebot <command> [bundlePath]');
+  console.log('  command     command to run ("update" or "check")');
+  console.log('  bundlePath  JSON file to update or compare against');
+  console.log('                (defaults to $CWD/licenses.json)');
+};
 
 /**
+ * @param bundlePath {string}
  * @returns {Promise<void>}
  */
-const update = async () => {
+const update = async (bundlePath) => {
+  const licensesPath = bundlePath || defaultBundlePath;
   const licenses = await getLicenses();
   const serialized = JSON.stringify(licenses, null, 2);
   await fs.writeFile(licensesPath, serialized, 'utf-8');
 };
 
 /**
+ * @param bundlePath {string}
  * @returns {Promise<void>}
  */
-const check = async () => {
+const check = async (bundlePath) => {
+  const licensesPath = bundlePath || defaultBundlePath;
   const licenses = await getLicenses();
   const serialized = JSON.stringify(licenses, null, 2);
   const fromDisk = await fs.readFile(licensesPath, 'utf-8');
@@ -26,12 +39,18 @@ const check = async () => {
 };
 
 const main = async () => {
-  switch (process.argv[2]) {
+  const [command, bundlePath] = process.argv.slice(2);
+  switch (command) {
+    case 'help':
+    case '--help':
+    case '-h':
+      return help();
     case 'update':
-      return update();
+      return update(bundlePath);
     case 'check':
-      return check();
+      return check(bundlePath);
     default:
+      help();
       throw new Error('Expected command as argument');
   }
 };
